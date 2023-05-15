b090323cefb56207e7db1edf580b288073f2f756
Abel Marquele <aamarquele@gmail.com>
2023-05-15 17:15:05 +0200

    espero que funciona

--- apiJhpiego/api/views.py
+++ apiJhpiego/api/views.py
@@ -76,10 +76,38 @@ def home(request):
 
 #         return HttpResponse('success', status=200)
         
-from .functions import *
+# from .functions import *
+
 import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import requests
+
+WHATSAPP_URL = 'https://graph.facebook.com/v16.0/103297242770340/messages'
+WHATSAPP_TOKEN = 'Bearer EAACwv64kYI4BAHaWJwUUzZBMA93EAKO4ygH0N6w2FikZBhRfQcpRG10o6ZBBkUGrj6tzInl1Smtfni8XJSvkjaJ8NzDoKZBYaiCAaoZAlPhtslIKfbedwS2SxdumOIi8HxjSkZC5jCwoIosqhsQLp3tLBtvWfGBgr7VfGcoUrc0BRv1flSdTf2RPgbhk7QgtqbipzyECpOZB30oo653VJNP'
+
+def sendWhatsAppMessage(phoneNumber, message):
+    headers = {"Authorization": WHATSAPP_TOKEN}
+    payload = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phoneNumber,
+        "type": "text",
+        "text": {"body": message}
+    }
+    response = requests.post(WHATSAPP_URL, headers=headers, json=payload)
+    if response.status_code == 200:
+        return "Mensagem enviada com sucesso!"
+    else:
+        return "Erro ao enviar mensagem: " + response.text
+
+
+# Exemplo de uso:
+phoneNumber = "258844680366"
+message = "Olá! Esta é uma mensagem de teste."
+result = sendWhatsAppMessage(phoneNumber, message)
+print(result)
+
 
 @csrf_exempt
 def whatsappWebhook(request):
