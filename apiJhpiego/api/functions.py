8d489f5a9e037ee8a27441330b6787f95a448d45
Abel Marquele <aamarquele@gmail.com>
2023-05-15 17:07:22 +0200

    Alteracoes usando o chatgbt

--- apiJhpiego/api/functions.py
+++ apiJhpiego/api/functions.py
@@ -1,27 +1,50 @@
-from django.conf import settings
+# from django.conf import settings
+# import requests
+
+
+# def sendWhatsAppMessage(phoneNumber, messege):
+#     headers = {"Authorization": settings.WHATSAPP_TOKEN}
+#     payload = {
+#                 "messaging_product": "whatsapp",
+#                 "recipient_type": "individual",
+#                 "to": phoneNumber, 
+#                 "type": "text", 
+#                 "text": { "body":messege} 
+
+#                }
+#     response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
+#     ans = response.json()
+#     print('111. headers',headers)
+#     print('222. payload',payload)
+#     print('333. response',response)
+#     print('444. ans',ans)
+#     return ans
+
 import requests
 
+WHATSAPP_URL = 'https://graph.facebook.com/v16.0/103297242770340/messages'
+WHATSAPP_TOKEN = 'Bearer EAACwv64kYI4BANuE0cOJyuBvCuGywNtGg39zO54bRVSuBWQLzZBNd0IhpPvPR1U9qNbPOxiiaUddQPANY0ZCrQn88NlEALzLtDsDr366UMwFdjRIjuVf9IWpui3GUncLGWdn4I8Lpc8tVGdBZC7GxcV9HIZBradKfG23u4gCakR0YOpC9ZB8uc2EQ6ciSsJoLc3CIXS3QZAzI9brQDEAv9'
 
-
-def sendWhatsAppMessage(phoneNumber, messege):
-    headers = {"Authorization": settings.WHATSAPP_TOKEN}
+def sendWhatsAppMessage(phoneNumber, message):
+    headers = {"Authorization": WHATSAPP_TOKEN}
     payload = {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": phoneNumber, 
-                "type": "text", 
-                "text": { "body":messege} 
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phoneNumber,
+        "type": "text",
+        "text": {"body": message}
+    }
+    response = requests.post(WHATSAPP_URL, headers=headers, json=payload)
+    if response.status_code == 200:
+        return "Mensagem enviada com sucesso!"
+    else:
+        return "Erro ao enviar mensagem: " + response.text
 
-               }
-    response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
-    ans = response.json()
-    return ans
 
-
-    # phoneNumber = "258844680366"
-
-    # messege = "Ola Abel, n\ Essa mensagem é de teste"
-
-    # sendWhatsAppMessage(phoneNumber, messege)
+# Exemplo de uso:
+phoneNumber = "258844680366"
+message = "Olá! Esta é uma mensagem de teste."
+result = sendWhatsAppMessage(phoneNumber, message)
+print(result)
 
 
