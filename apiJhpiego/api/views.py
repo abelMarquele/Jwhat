516fe931203979ca886f702a816888cd1df9b6c3
Abel Marquele <aamarquele@gmail.com>
2023-05-18 14:00:01 +0200

    teste de menu 8

--- apiJhpiego/api/views.py
+++ apiJhpiego/api/views.py
@@ -4,25 +4,21 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
 
-
-
 WHATSAPP_URL = 'https://graph.facebook.com/v16.0/103297242770340/messages'
 
-def sendWhatsAppMessage(phoneNumber, message):
+def send_message(phone_number, message):
+    # token = 'SEU_TOKEN_AQUI'  # Substitua pelo seu token de acesso do WhatsApp
     token = config('WHATSAPP_TOKEN', default='')
-
-    if not token:
-        return "Erro: Token de acesso não encontrado."
-
     headers = {"Authorization": token}
     payload = {
         "messaging_product": "whatsapp",
         "recipient_type": "individual",
-        "to": phoneNumber,
+        "to": phone_number,
         "type": "text",
         "text": {"body": message}
     }
     response = requests.post(WHATSAPP_URL, headers=headers, json=payload)
+    
     if response.status_code == 200:
         ans = response.json()
         return ans
@@ -30,14 +26,14 @@ def sendWhatsAppMessage(phoneNumber, message):
         return "Erro ao enviar mensagem: " + response.text
 
 
-def send_menu_message(phoneNumber, menu_options):
+def send_menu_message(phone_number, menu_options):
     menu_title = "MISAU - Menu de Opções"
     menu_message = "{}\n\n{}".format(menu_title, "\n".join(menu_options))
 
-    sendWhatsAppMessage(phoneNumber, menu_message)
+    send_message(phone_number, menu_message)
 
 
-def handle_menu_choice(phoneNumber, choice):
+def handle_menu_choice(phone_number, choice):
     response_message = ""
 
     if choice == '1':
@@ -49,13 +45,13 @@ def handle_menu_choice(phoneNumber, choice):
     else:
         response_message = "Opção inválida. Por favor, escolha uma opção válida."
 
-    sendWhatsAppMessage(phoneNumber, response_message)
+    send_message(phone_number, response_message)
 
     secondary_menu_options = [
         "1. Voltar ao Menu Anterior",
         "0. Terminar Conversa"
     ]
-    send_menu_message(phoneNumber, secondary_menu_options)
+    send_menu_message(phone_number, secondary_menu_options)
 
 
 @csrf_exempt
@@ -65,13 +61,13 @@ def whatsappWebhook(request):
 
         if 'object' in data and 'entry' in data:
             if data['object'] == 'whatsapp_business_account':
-                try:
-                    for entry in data['entry']:
+                for entry in data['entry']:
+                    try:
                         phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
                         text = entry['changes'][0]['value']['messages'][0]['text']['body']
 
                         if text == '0':
-                            sendWhatsAppMessage(phoneNumber, "Conversa encerrada. Obrigado!")
+                            send_message(phoneNumber, "Conversa encerrada. Obrigado!")
                         elif text == '1' or text == '2' or text == '3':
                             handle_menu_choice(phoneNumber, text)
                         elif text == '9':
@@ -83,14 +79,12 @@ def whatsappWebhook(request):
                             ]
                             send_menu_message(phoneNumber, main_menu_options)
                         else:
-                            sendWhatsAppMessage(phoneNumber, "Opção inválida. Por favor, escolha uma opção válida.")
+                            send_message(phoneNumber, "Opção inválida. Por favor, escolha uma opção válida.")
 
-                except:
-                    pass
-
+                    except Exception as e:
+                        print(f"Erro ao processar mensagem: {e}")
+        
         return HttpResponse('success', status=200)
 
     elif request.method == 'GET':
         return HttpResponse("Webhook configurado com sucesso!")
-
-
